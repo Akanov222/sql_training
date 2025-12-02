577abbf30383279b701cdd9d76f4882a93b97f41
Akanov222 <akanov.gazproekt@gmail.com>
2025-12-03 01:42:06 +0300

    Include 6 task

--- src/sql1/1-5_exersises.sql
+++ src/sql1/1-5_exersises.sql
@@ -15,11 +15,12 @@ FROM employees AS e
 JOIN departments AS d ON d.department_id = e.department_id
 WHERE d.department_name = 'HR';
 
-SELECT d.department_id, d.department_name, ROUND(AVG(e.salary)) AS avaradge_salary
+SELECT d.department_name, d.department_id, ROUND(AVG(e.salary)) AS avarage_salary
 FROM employees AS e
-         JOIN departments AS d ON e.department_id = d.department_id
-GROUP BY d.department_id
-ORDER BY d.department_id ASC;
+         JOIN departments AS d
+              ON e.department_id = d.department_id
+GROUP BY department_name, d.department_id
+ORDER BY avarage_salary DESC;
 
 -- 4. Найти сотрудников, чья фамилия начинается на 'Ив'
 SELECT *
@@ -30,3 +31,4 @@ WHERE last_name LIKE 'Ив%';
 SELECT *
 FROM employees
 WHERE hire_date >= CURRENT_DATE - INTERVAL '3 year';
+
